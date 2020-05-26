c299a3f827df30efc4ef79874ef3e41dcbc8c01e
WilliamJGrace <williamjgrace@outlook.com>
2020-05-26 17:17:12 +0100

    added controller and routes fodlers and files

--- server/index.js
+++ server/index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 
+const db = require('./db')
+const movieRouter = require('./routes/movie-router')
+
 
 const apiPort = 3000
 
