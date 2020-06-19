ca0f2191febb2265fa2d9755bed3af81b70c3900
WilliamJGrace <williamjgrace@outlook.com>
2020-06-19 10:07:51 +0100

    user can delete a movie and click update

--- client/src/pages/MoviesList.jsx
+++ client/src/pages/MoviesList.jsx
@@ -7,8 +7,46 @@ import styled from 'styled-components'
 const Wrapper = styled.div`
     padding: 0 40px 40px 40px;
 `
+const Update = styled.div`
+    color: #ef9b0f;
+    cursor: pointer;
+`
+
+const Delete = styled.div`
+    color: #ff0000;
+    cursor: pointer;
+`
 
 
+class UpdateMovie extends Component {
+    updateUser = event => {
+        event.preventDefault()
+
+        window.location.href = `/movies/update/${this.props.id}`
+    }
+
+    render() {
+        return <Update onClick={this.updateUser}>Update</Update>
+    }
+}
+class DeleteMovie extends Component {
+    deleteUser = event => {
+        event.preventDefault()
+
+        if (
+            window.confirm(
+                `Do tou want to delete the movie ${this.props.id} permanently?`,
+            )
+        ) {
+            api.deleteMovieById(this.props.id)
+            window.location.reload()
+        }
+    }
+
+    render() {
+        return <Delete onClick={this.deleteUser}>Delete</Delete>
+    }
+}
 
 class MoviesList extends Component {
     constructor(props) {
@@ -55,6 +93,28 @@ class MoviesList extends Component {
                 accessor: 'time',
                 Cell: props => <span>{props.value.join(' / ')}</span>,
             },
+            {
+                Header: '',
+                accessor: '',
+                Cell: function(props) {
+                    return (
+                        <span>
+                            <DeleteMovie id={props.original._id} />
+                        </span>
+                    )
+                },
+            },
+            {
+                Header: '',
+                accessor: '',
+                Cell: function(props) {
+                    return (
+                        <span>
+                            <UpdateMovie id={props.original._id} />
+                        </span>
+                    )
+                },
+            }
         ]
 
         let showTable = true
